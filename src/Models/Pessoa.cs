2b42087dc46a1127eee235a6121f3791458de997
unknown <vinicius.souza@novaxs.com.br>
2022-09-01 22:48:26 -0300

    Explorando Dotnet

--- src/Models/Pessoa.cs
+++ src/Models/Pessoa.cs
@@ -28,7 +28,7 @@ public class Pessoa
     public string Nome { get; set; }
     public int Idade { get; set; }
     public string? Cpf { get; set; }
-    public Boolean Ativado { get; set; }
+    public Boolean Ativado { get; set; } 
 
     public List<Contrato> contratos { get; set; }
     
