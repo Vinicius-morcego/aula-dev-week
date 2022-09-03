e76c85e1f9d728694f7933517ec5b79cdab13d90
unknown <vinicius.souza@novaxs.com.br>
2022-09-03 17:39:59 -0300

    Finalizando a devweek com uma API para Seguradora, let´s go!

--- src/Controllers/PersonController.cs
+++ src/Controllers/PersonController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
-using src.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+using src.Models;
 using src.Persistence;
 
-
 namespace src.Controllers;
+
 [ApiController]
 [Route("[controller]")]
 public class PersonController : ControllerBase{
@@ -16,41 +18,90 @@ public class PersonController : ControllerBase{
     }
     
     [HttpGet]
-    public List<Pessoa> Get(){
+    public ActionResult<List<Pessoa>> Get(){
         // var pessoa = new Pessoa("vinicius", 38, "00000000000");
         // var contrato = new Contrato("abc123", 50.20);
 
         //pessoa.contratos.Add(contrato);
+        var result = _repository.Pessoas.Include(p => p.contratos).ToList();
 
-        return _repository.Pessoas.Include(p => p.contratos).ToList();
+        if(!result.Any()) return NoContent();        
 
+        return Ok(result);
         //return pessoa;
     }
 
     [HttpPost]
-    public Pessoa Post([FromBody] Pessoa pessoa)
+    public ActionResult<Pessoa> Post([FromBody] Pessoa pessoa)
     {
-        _repository.Pessoas.Add(pessoa);
-        _repository.SaveChanges();
-        return pessoa;
+        try
+        {
+            _repository.Pessoas.Add(pessoa);
+            _repository.SaveChanges();            
+        }
+        catch (System.Exception)
+        {
+            return BadRequest(new {
+                msg = "Houve erro ao enviar solicitação",
+                status = HttpStatusCode.BadRequest
+            });
+        }
+        return Created("criado", pessoa);
     }
 
     [HttpPut("{id}")]
-    public string Update([FromRoute]int id, [FromBody] Pessoa pessoa)
+    public ActionResult<Object> Update(
+        [FromRoute]int id, 
+        [FromBody] Pessoa pessoa)
     {
+
+        var result = _repository.Pessoas.SingleOrDefault(e => e.Id == id);
+        if(result is null)
+        {
+            return NotFound(new 
+            {
+                msg = "Não encontrado!",
+                status = HttpStatusCode.NotFound
+            });
+        }
+        try
+        {
         _repository.Pessoas.Update(pessoa);
-        _repository.SaveChanges();
-        Console.WriteLine(id);
-        Console.WriteLine(pessoa);
-        return "Dados do id " + id + " atualizados";
+        _repository.SaveChanges();            
+        }
+        catch (System.Exception)
+        {
+            
+            return BadRequest(new{
+                msg = "Houve erro ao enviar solicitação de atualização do "
+                + id + "atualizados",
+                status = HttpStatusCode.BadRequest
+            });
+        }
+        return Ok(new{
+           msg = $"Dados do id {id} atualizados",
+           status = HttpStatusCode.OK
+        });
+
+        
     }
 
     [HttpDelete("{id}")]
-    public string Delete([FromRoute] int id)
+    public ActionResult<Object> Delete([FromRoute] int id)
     {
         var result = _repository.Pessoas.SingleOrDefault(e => e.Id == id);
+        if(result is null) return BadRequest(new {
+            msg = "Conteúdo inexistente, solicitação inválida",
+            status = HttpStatusCode.BadRequest
+        });
+        
         _repository.Pessoas.Remove(result);
         _repository.SaveChanges();
-        return "deletado pessoa de Id "+id;
+        return Ok(new {
+            msg = "deletado pessoa de Id "+id,
+            status = HttpStatusCode.OK
+    });
     }
-}
+  
+ }
+
---
+++ src/Enums/Tipos.cs
@@ -0,0 +1,8 @@
+namespace Aula_dev_week.src
+{
+    public enum Tipos
+    {
+        ativado = 100,
+        desativado = 200
+    }
+}
